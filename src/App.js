f77e15f1891c3123c4acfcc5da911b24cac46cbc
Christian/Tristan <christiandunne18@gmail.com>
2025-10-22 20:53:08 -0500

    final merge conflicts fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
-import Main from "./Components/Main";
-=======
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import ClassMain from "./Components/ClassMain";
 import ClassList from "./Components/ClassList";
 import StudentMain from "./Components/StudentMain";
 import StudentList from "./Components/StudentList";
->>>>>>> student-b
 import * as Env from "./environments";
 import Parse from "parse";
 
@@ -16,9 +12,6 @@ Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 Parse.serverURL = Env.SERVER_URL;
 
 function App() {
-<<<<<<< HEAD
-  return <Main />;
-=======
   return (
     <Router>
       <div className="App">
@@ -42,7 +35,6 @@ function App() {
       </div>
     </Router>
   );
->>>>>>> student-b
 }
 
 export default App;
--- src/environments.js
+++ src/environments.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-// this is ours. once we have a back4app this is the one to use
-// module.exports = {
-//     APPLICATION_ID: "wo3KDh7NCtauPdkjC8tT7iKjyyz8M9pqJRs8yoJN",
-//     JAVASCRIPT_KEY: "3P9m47h1fV61PsAttp5MHbV5TCU0VwtPaWBWHnyU",
-//     SERVER_URL: "https://parseapi.back4app.com/"
-// }
-
-// this is the stuff from the feature 4 kickoff solution. not linked to our back4app
-module.exports = {
-  APPLICATION_ID: "LG9Z5QPaxemHcp1R5ENzej17Dt4FVFmnZaGhdJy5",
-  JAVASCRIPT_KEY: "LF6yM7u7jES772l9wLgpRacpjng5H1WuvQIpcDEs",
-  SERVER_URL: "https://parseapi.back4app.com/",
-=======
 module.exports = {
   APPLICATION_ID: "wo3KDh7NCtauPdkjC8tT7iKjyyz8M9pqJRs8yoJN",
   JAVASCRIPT_KEY: "3P9m47h1fV61PsAttp5MHbV5TCU0VwtPaWBWHnyU",
   SERVER_URL: "https://parseapi.back4app.com",
->>>>>>> student-b
 };
